50f1959fb94f1ff4d838b2ea98e6b77c2c4b00af
Christopher <Ccharrington11@gmail.com>
2025-09-09 05:30:48 -0400

    updates to express, resolving build error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
+import express from "express";
 
+const app = express();
+const port = 3000;
+app.get("/api", (req, res) => {
+    res.send('Server Online');
+});
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
 const Greeting = "Hello User";
 
 console.log(Greeting);
